11d34ffc2a4a9610c4ea79ef979dc3e890a87dd0
Michael Bao <tcmb139@gmail.com>
2024-12-12 22:35:16 -0500

    add transition between webpages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,21 @@
 <script lang="ts">
+	import { onNavigate } from '$app/navigation';
+
 	import '../app.css';
 	const { children } = $props();
+
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
-{@render children()}
+<div class="h-full w-full">
+	{@render children()}
+</div>
