6543f6c86cc844f08c081adc2aba3dadfca8b715
Michael Bao <tcmb139@gmail.com>
2025-03-01 16:18:30 -0500

    fixed background on smaller screens

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,7 +17,7 @@
 </script>
 
 <div class="h-full w-full">
-	<img src="/wallpaper.png" alt="Wallpaper" class="absolute -z-[999] object-cover" />
+	<img src="/wallpaper.png" alt="Wallpaper" class="absolute -z-[999] h-full object-cover" />
 	<div class="relative h-full w-full overflow-clip">
 		{@render children()}
 	</div>
